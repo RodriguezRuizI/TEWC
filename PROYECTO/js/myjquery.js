76851d6af8d1517669f8e2d911504796240baa06
RodriguezRuizI <33327123+RodriguezRuizI@users.noreply.github.com>
2018-02-13 18:25:15 +0100

    Add files via upload

---
+++ PROYECTO/js/myjquery.js
@@ -0,0 +1,12 @@
+/**
+ * Created by RodríguezRuiz on 08/04/2016.
+ */
+
+
+ $(function(d, s, id) {
+            var js, fjs = d.getElementsByTagName(s)[0];
+            if (d.getElementById(id)) return;
+            js = d.createElement(s); js.id = id;
+            js.src = "//connect.facebook.net/es_ES/sdk.js#xfbml=1&version=v2.5";
+            fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));
